ddbb58e4b2fc5d4ec56106914811ee7128312584
carrainme <130687012+carrainme@users.noreply.github.com>
2023-06-02 13:03:33 +0800

    Update default.ts
    更新环境变量

--- src/default.ts
+++ src/default.ts
@@ -8,5 +8,5 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 欢迎来到待开的网页，我只做chatgpt问答转发，仅供娱乐和学习，在输入框输入你的问题吧
-
+     
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
